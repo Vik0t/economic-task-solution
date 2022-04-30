237c8ba25140775f22558b62e4aa061a2458770c
Vik0t <viktorvarrar@gmail.com>
2022-04-30 19:31:40 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Calculate(Console.ReadLine()));
+        }
+        static double Calculate(string input)
+        {
+            string[] elements = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            double value = double.Parse(elements[0]);
+            double percent = double.Parse(elements[1]);
+            double days = double.Parse(elements[2]);
+
+            double summary = value * Math.Pow(percent / 100 / 12 + 1, days);
+            return summary;
+        }
+    }
+}
